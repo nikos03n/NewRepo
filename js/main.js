31318976d5ca4a1114da188a474e1e5f580ca8d4
nikolay tamazenko <tamazenko@outlook.com>
2020-06-15 21:49:59 +0300

    Урок 2.19. Динамическая типизация в JS

--- js/main.js
+++ js/main.js
@@ -1,94 +1,40 @@
-'use strict';
+//console.log(typeof (String(4)));
 
-let money, time;
+console.log(typeof ("" + false));
 
-function start() {
-    money = +prompt("Ваш бюджет на месяц?", "");
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+console.log('http://f0433670.xsph.ru/' + 5);
 
-    while (isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц?", "");
-    }
 
+////приобразоваваем числа----динамическая типизация
+//1
+console.log(typeof (Number('5')));//v namber
+
+//2вариант
+console.log(typeof (+'5')); //перед любым типом данных ставим + приврящает в число
+console.log(typeof (5 + +'5'));//v namber
+
+//3
+
+console.log(typeof (parseInt('15px', 10))); //v namber
+
+let ans = +prompt('Helloy', '');//добавили + и все что получ от пользоватиля будит числом
+
+// то что привращается в неправду
+//0, '', null, undefined, nan  //всегда неправда а если 'пробел' то это значение
+//1
+let switcher = null;
+
+if (switcher) {
+    console.log('Working')
 }
-start();
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true,
-    chooseExpenses: function () {
-        for (let i = 0; i < 2; i++) {
-            let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-                b = prompt("Во сколько обойдется?", "");
+switcher = 1
 
-            if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
-
-                console.log("done");
-
-                appData.expenses[a] = b;
-            } else {
-                console.log("bad result");
-                i--;
-            }
-
-        }
-    },
-    detectDayBudget: function () {
-        appData.moneyPerDay = (appData.budget / 30).toFixed();
-        alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
-    },
-    detectLevel: function () {
-        if (appData.moneyPerDay < 100) {
-            console.log("Это минимальный уровень достатка!");
-        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-            console.log("Это средний уровень достатка!");
-        } else if (appData.moneyPerDay > 2000) {
-            console.log("Это высокий уровень достатка!");
-        } else {
-            console.log("Ошибочка...!");
-        }
-    },
-    checkSavings: function () {
-        if (appData.savings == true) {
-            let save = +prompt("Какова сумма накоплений?"),
-                percent = +prompt("Под какой процент?");
-
-            appData.monthIncome = save / 100 / 12 * percent;
-            alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
-        }
-    },
-    chooseOptExpenses: function () {
-        for (let i = 1; i <= 3; i++) {
-            let questionOptExpenses = prompt("Статья необязательных расходов?");
-            appData.optionalExpenses[i] = questionOptExpenses;
-            console.log(appData.optionalExpenses);
-        }
-    },
-    chooseIncome: function () {
-
-        let items = prompt("Что принесет дополнительный доход? (Перечислите через запятую)", "");
-
-        if (typeof (items) != "string" || items == "" || typeof (items) == null) {
-            console.log("Вы ввели некорректные данные или не ввели их вовсе");
-        } else {
-            appData.income = items.split(", ");
-            appData.income.push(prompt("Может что-то еще?"));
-            appData.income.sort();
-        }
-
-        appData.income.forEach(function (itemmassive, i) {
-            alert("Способы доп. заработка: " + (i + 1) + " - " + itemmassive);
-        });
-
-    }
-
-
-};
-
-for (let key in appData) {
-    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
+if (switcher) {
+    console.log('Working')
 }
+//2
+console.log(typeof (Boolean('5')));
+//3
+
+
