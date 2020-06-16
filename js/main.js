5a9ccafe88fdd1b861b85d4d69143eff35d3482c
nikolay tamazenko <tamazenko@outlook.com>
2020-06-16 22:36:11 +0300

    12121212121

--- js/main.js
+++ js/main.js
@@ -4,7 +4,4 @@ let box = document.getElementById('box'),
     heart = document.querySelectorAll('.heart'),
     oneHeart = document.querySelector('.heart');
 
-dox.style.backgroundColor
-
-
-
+dox.style.backgragdghdfhdd
