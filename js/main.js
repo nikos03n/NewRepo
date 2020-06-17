85e098cee3e6926de43d5f29f80722be178b9b83
nikolay tamazenko <tamazenko@outlook.com>
2020-06-17 19:02:07 +0300

    Урок 2.23. Действия с элементами на странице00,1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
-let box = document.getElementById('box'),
-    btn = document.getElementsByTagName('button'),
-    circle = document.getElementsByClassName('circle'),
-    heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+let box = document.getElementById('box')
+btn = document.getElementsByTagName('button')
+circle = document.getElementsByClassName('circle')
+heart = document.querySelectorAll('.heart')
+oneHeart = document.querySelector('.heart')
 
-dox.style.backgragdghdfhdddfhergrwgdfdfffsgrw1111111111111111111
+box.style.backgroundColor = 'blue'
+btn[3].style.borderRadius = '100%'
+
+circle[0].style.backgroundColor = '#ccff00'
+circle[1].style.backgroundColor = 'rgb(0, 255, 31)'
+circle[2].style.backgroundColor = 'rgb(230, 16, 75)'
+
+
+//цикл-----------------------------------1, 2
+// for (let i = 0; i < heart.length; i++) {
+// heart[i].style.backgroundColor = 'blue'
+// }
+
+
+//1-moi - это каждая конфета которую попроб, 
+//2-i это номер конфеты, 
+//3moi2 - это все конфетты 
+// heart.forEach(function (moi, i, moi2) {
+// moi.style.backgroundColor = 'black'
+// })
+
+//----------------создать элемент
+let div = document.createElement('div')
+text = document.createTextNode('тутбыл я')
+
+div.classList.add('black')
+
+document.body.appendChild(div) //вывели на сайт
+
+console.log(div)
