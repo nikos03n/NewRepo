49507176e4e69d39900cbf173e126bdd74de8214
nikolay tamazenko <tamazenko@outlook.com>
2020-06-19 20:28:19 +0300

    Урок 2.26. События и их обработчики 0,1

--- js/main.js
+++ js/main.js
@@ -1,57 +1,38 @@
-let box = document.getElementById('box')
-btn = document.getElementsByTagName('button')
-circle = document.getElementsByClassName('circle')
-heart = document.querySelectorAll('.heart')
-oneHeart = document.querySelector('.heart')
-wrapper = document.querySelector('.wrapper')
-
-//console.log(oneHeart)
-
-
-box.style.backgroundColor = 'blue'
-btn[3].style.borderRadius = '100%'
-
-circle[0].style.backgroundColor = '#ccff00'
-circle[1].style.backgroundColor = 'rgb(0, 255, 31)'
-circle[2].style.backgroundColor = 'rgb(230, 16, 75)'
-
-
-//цикл-----------------------------------1, 2
-// for (let i = 0; i <h1 heart.length; i++) {
-// heart[i].style.backgroundColor = 'blue'
+let btn = document.getElementsByTagName('button')
+wrap = document.querySelector('.wrapper')
+link = document.querySelector('a')
+// btn[0].onclick = function () {
+//     alert('вы нажали первую кнопку')
 // }
+// btn[0].addEventListener('click', function () {
+//     alert('вы нажали первую кнопку')
+//     alert('вы опять нажали первую кнопку')
+// })
+//---------------
+// btn[0].addEventListener('click', function () {
+//     alert('вы нажали первую кнопку')
+// })
+// btn[0].addEventListener('click', function () {
+//     alert('вы опять нажали первую кнопку')
+// })
+//----------------------------
+// btn[0].addEventListener('mouseenter', function () {
+//     alert('Вы навели на первую кнопку, вылазит при наведении')
+// })
+//-------------------------------event объект тоесть событие 
+btn[0].addEventListener('click', function (event) {
+    console.log(event)
+    let target = event.target
+    target.style.display = 'none'
+    // console.log('произошло событие: ' + event.type + ' на элименте ' + event.target)
+})
 
-
-//1-moi - это каждая конфета которую попроб, 
-//2-i это номер конфеты, 
-//3moi2 - это все конфетты1 
-// heart.forEach(function (moi, i, moi2) {
-// moi.style.backgroundColor = 'black'
+// wrap.addEventListener('click', function () {
+//     console.log('произошло событие: ' + event.type + ' на элименте ' + event.target)
 // })
 
-//----------------создать элемент
-let div = document.createElement('div')
-text = document.createTextNode('тутбыл я')
-div.textContent = 'heloy'
-div.classList.add('black')
-console.log(div)
-//div.innerHTML = 'heloy word'
-
-//dfocument.body.appendChild(div) //вывели на сайт
-//wrapper.appendChild(div) //вывел в конец wrapper
-document.body.insertBefore(div, circle[1]) //вывел куда надо
-document.body.removeChild(circle[2]) //удаляет элемент
-wrapper.removeChild(heart[1])
-
-//замена одного на другой элемент
-//document.body.replaceChild(btn[1], circle[1]);
-//wrapper.replaceChild(btn[0], heart[2]);
-//wrapper.replaceChild(btn[1], heart[0]);
-
-//--------------добовляем текст в htm.l----
-// div.innerHTML = '<h1>heloy word < /h1>'
-// document.body.insertBefore(div, circle[1]) //вывел куда надо
-//div.textContent = 'heloy'
-
-
-//console.log(div)
+//сылка делается не переходной
+link.addEventListener('click', function (event) {
+    event.preventDefaunt()
+    console.log('произошло событие: ' + event.type + ' на элименте ' + event.target)
+})
