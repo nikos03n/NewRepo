9a8e9905d5d4b8092c486dd6105dcafa3b98f1c1
nikolay tamazenko <tamazenko@outlook.com>
2020-06-20 21:54:31 +0300

    Урок 2.28. событие на моб регулярных выражени

--- 2.25/src/js/main.js
+++ 2.25/src/js/main.js
@@ -1,138 +1,61 @@
-let startBtn = document.getElementById("start"),
-	budgetValue = document.getElementsByClassName('budget-value')[0],
-	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
-	levelValue = document.getElementsByClassName('level-value')[0],
-	expensesValue = document.getElementsByClassName('expenses-value')[0],
-	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
-	incomeValue = document.getElementsByClassName('income-value')[0],
-	monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
-	yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+Let box = document.querySelector('.box');
 
+// box.addEventListener('touchstart', function (e) {
+// 	e.preventDefault();
+// 	console.log('Red box: touchstart');
+// 	console.log(e.target);
+// 	console.log(e.touches[0].target);
+// 	console.log(e.changedTouches);
+// 	console.log(e.targetTouches);
 
-	expensesItem = document.getElementsByClassName('expenses-item'),
-	expensesBtn = document.getElementsByTagName('button')[0],
-	optionalExpensesBtn = document.getElementsByTagName('button')[1],
-	countBtn = document.getElementsByTagName('button')[2],
-	optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
-	incomeItem = document.querySelector('.choose-income'),
-	checkSavings = document.querySelector('#savings'),
-	sumValue = document.querySelector('.choose-sum'),
-	percentValue = document.querySelector('.choose-percent'),
-	yearValue = document.querySelector('.year-value'),
-	monthValue = document.querySelector('.month-value'),
-	dayValue = document.querySelector('.day-value');
-
-let money, time;
-
-expensesBtn.disabled = true;
-optionalExpensesBtn.disabled = true;
-countBtn.disabled = true;
-
-startBtn.addEventListener('click', () => {
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
-	money = +prompt("Ваш бюджет на месяц?", '');
-
-	while (isNaN(money) || money == '' || money == null) {
-		money = prompt("Ваш бюджет?", "");
-	}
-	appData.budget = money;
-	appData.timeData = time;
-	budgetValue.textContent = money.toFixed();
-	yearValue.value = new Date(Date.parse(time)).getFullYear();
-	monthValue.value = new Date(Date.parse(time)).getMonth() + 1;
-	dayValue.value = new Date(Date.parse(time)).getDate();
-
-	expensesBtn.disabled = false;
-	optionalExpensesBtn.disabled = false;
-	countBtn.disabled = false;
+box.addEventListener('touchmove', function (e) {
+	e.preventDefault();
+	console.log("Red box: " + e.touches[0].pageX);
 });
 
-expensesBtn.addEventListener('click', () => {
-	let sum = 0;
-	for (let i = 0; i < expensesItem.length; i++) {
-		let a = expensesItem[i].value,
-			b = expensesItem[++i].value;
 
-		if ((typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-			appData.expenses[a] = b;
-			sum += +b;
-		} else {
-			i = i - 1;
-		}
-		expensesValue.textContent = sum;
-	}
-});
 
-optionalExpensesBtn.addEventListener('click', () => {
-	for (let i = 0; i < optionalExpensesItem.length; i++) {
-		let opt = optionalExpensesItem[i].value;
-		appData.optionalExpenses[i] = opt;
-		optionalExpensesValue.textContent += appData.optionalExpenses[i] + ' ';
-	}
-});
 
-countBtn.addEventListener('click', () => {
-	if (appData.budget != undefined) {
-		appData.moneyPerDay = ((appData.budget - +expensesValue.textContent) / 30).toFixed();
-		dayBudgetValue.textContent = appData.moneyPerDay;
-		if (appData.moneyPerDay < 100) {
-			levelValue.textContent = 'Минимальный уровень достатка';
-		} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-			levelValue.textContent = 'Средний уровень достатка';
-		} else if (appData.moneyPerDay > 2000) {
-			levelValue.textContent = 'Высокий уровень достатка';
-		} else {
-			levelValue.textContent = 'Произошла ошибка';
-		}
-	} else {
-		dayBudgetValue.textContent = 'Произошла ошибка';
-	}
-});
 
-incomeItem.addEventListener('input', () => {
-	let items = incomeItem.value;
-	console.log(1);
-	if (isNaN(items) || items != '') {
-		appData.income = items.split(',');
-		incomeValue.textContent = appData.income;
-	}
-});
+//-----------------ругулярные вырожения
 
-checkSavings.addEventListener("click", () => {
-	if (appData.savings == true) {
-		appData.savings = false;
-	} else {
-		appData.savings = true;
-	}
-});
-//---------------------------------
-sumValue.addEventListener('input', () => {
-	if (appData.savings == true) {
-		let sum = +sumValue.value;
-		let percent = +percentValue.value;
-		appData.monthIncome = sum / 100 / 12 * percent;
-		appData.yearIncome = sum / 100 * percent;
-		monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
-		yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
-	}
-});
+// /new ReqExp('pattern', 'flang');
+// /pattern/
 
-percentValue.addEventListener('input', () => {
-	if (appData.savings == true) {
-		let sum = +sumValue.value;
-		let percent = +percentValue.value;
-		appData.monthIncome = sum / 100 / 12 * percent;
-		appData.yearIncome = sum / 100 * percent;
-		monthSavingsValue.textContent = appData.monthIncome.toFixed(1);
-		yearSavingsValue.textContent = appData.yearIncome.toFixed(1);
-	}
-});
+// let ans = prompt('Введенме ваше имя');
 
-const appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
-};
+// let red = /n/i;
+
+// console.log(ans.search(reg));
+
+//лучше метод
+let ans = prompt('Введенме ваше имя');
+
+let red = /n/gi;
+
+console.log(ans.match(reg));
+
+//-------
+//i
+//g
+//m
+let pass = promt('Введите пароль');
+
+console.log(pass.replace(/./g, "*"));
+alert('12-34-56'.replace(/-/g, ':'))
+
+
+
+//----------ищим глобально все цифры которые будут в этой строке /\d/g;
+//let ans = prompt('Введение число');
+
+//let reg = /\d/g;
+
+//console.log(ans.match(reg));
+//------------------
+
+let str = 'Me name is R2D2';
+
+console.log(str.match(/\w\d\w\d/i));
+//регулярные выражения позваляют найти все \водим здесь все\
+});
