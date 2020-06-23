285c1af1ed9c9f4e7906011e7e710862220d573b
nikolay tamazenko <tamazenko@outlook.com>
2020-06-23 09:59:23 +0300

    Урок 3.7. Контекст вызова (this)

--- 3,2/js/script.js
+++ 3,2/js/script.js
@@ -1,109 +1,105 @@
-// window.addEventListener('DOMContentLoaded', function () {
+// //в данном случии this как бомж привязон к окну виндавс
+// // function showThis() {
+// //     console.log(this);
+// // }
+// // showThis()
+// // //-------------------это неправельно 
+// // function showThis(a, b) {//доб два аргумента
+// //     console.log(this);//консол оставим чтобы смотреть
+// //     function sum() {//внутрь добавим еще одну функцию
+// //         console.log(this);
+// //         return this.a + this.b;//должна вернуть а+b которые передаются showThis
+// //     }
+// //     console.log(sum());//смотрим
+// // }
+// // showThis(4, 5);
+// // showThis(5, 5);
 
-//     'use strict';
-//     let tab = document.querySelectorAll('.info-header-tab'),
-//         info = document.querySelector('.info-header'),
-//         tabContent = document.querySelectorAll('.info-tabcontent');
+// //-------------------ПРАВЕЛЬНО делать нужно ЗАМЫКАНИЕ
+// // function showThis(a, b) {            //доб два аргумента
+// //     console.log(this);           //консол оставим чтобы смотреть
+// //     function sum() {            //внутрь добавим еще одну функцию
+// //         console.log(this);
+// //         return a + b;//            (ЗАМЫКАНИЕ)всебе a b не находит ищит выше showThis здесь она видит что есто два параметра showThis(4, 5);
+// //     }
+// //     console.log(sum());             //смотрим
+// // }
+// // showThis(4, 5);
+// // showThis(5, 5);
 
-//     function hideTabContent(a) {
-//         for (let i = a; i < tabContent.length; i++) {
-//             tabContent[i].classList.remove('show');
-//             tabContent[i].classList.add('hide');
+// //----------------------------
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function () {
+//         console.log(this);
+//         function shout() {
+//             console.log(this);
 //         }
+//         shout();
 //     }
+// };
+// obj.sum();//(получили наш объект). sum это функция но когда она является методом кокогото объекта то контекст выполнения это и есть сам объект 
 
-//     hideTabContent(1);
+// //---------РУЧНОЙ метод-передачи контекст вызова-----------------------------------
+// // let user = {
+// //     name: "John"
+// // };
 
-//     function showTabContent(b) {
-//         if (tabContent[b].classList.contains('hide')) {
-//             tabContent[b].classList.remove('hide');
-//             tabContent[b].classList.add('show');
-//         }
+// // function saName() {
+// //     console.log(this);
+// //     console.log(this.name);
+// // }
+// // console.log(saName.call(user));
+// // console.log(saName.apply(user));
+// //--------------------------
+// let user = {
+//     name: "John"
+// };
+
+// function saName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+// console.log(saName.call(user, 'Smith'));//строку передаем
+// console.log(saName.apply(user, 'Snow'));//масиф передаем
+
+// //-----------------------------------
+// function count(number) {
+//     return this * number;
+// }
+
+// let double = count.bind(2); //к this при помощи метода bind привязываем(2) если просто то то что передаем в bind всегда заменяет this
+// console.log(double(3));
+// console.log(double(10));
+
+
+//--Указание конкретного контекста - coll, apply, bind--------------------------------
+// let btn = document.querySelector('button');//ВЫЗВАЛИ КНОПКУ В КОНСОЛ
+
+// btn.addEventListener('click', function () {
+//     console.log(this);
+// })
+//----------------------
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    console.log(this)
+    this.style.backgroundColor = 'red'
+})
+
+// let btn = document.querySelector('button');
+
+// btn.addEventListener('click', function () {
+//     console.log(this)
+//     this.style.backgroundColor = 'red'
+//     function showThis() {
+//         console.log(this)
 //     }
+//     showThis()
+// })
 
-//     info.addEventListener('click', function (event) {
-//         let target = event.target;
-//         if (target && target.classList.contains('info-header-tab')) {
-//             for (let i = 0; i < tab.length; i++) {
-//                 if (target == tab[i]) {
-//                     hideTabContent(0);
-//                     showTabContent(i);
-//                     break;
-//                 }
-//             }
-//         }
-
-//     });
-
-//     // -----------Timer ---------------
-
-//     let deadline = '2020-06-23';//первый нунтк задали конечную дату
-
-//     function getTimeRemaining(endtime) {//сюда будим передовать эту дату
-//         let t = Date.parse(endtime) - Date.parse(new Date()),//в t лужит разница в милисик
-//             seconds = Math.floor((t / 1000) % 60),
-//             minutes = Math.floor((t / 1000 / 60) % 60),
-//             hours = Math.floor((t / (1000 * 60 * 60)));
-
-//         return {//возврвщает целый объект объект который содержит
-//             'total': t,
-//             'hours': hours,
-//             'minutes': minutes,
-//             'seconds': seconds
-//         };
-//     }
-
-//     function setClock(id, endtime) {//куда устанавливаем (setClockберет различные переменные со страници)
-//         let timer = document.getElementById(id),//
-//             hours = timer.querySelector('.hours'),
-//             minutes = timer.querySelector('.minutes'),
-//             seconds = timer.querySelector('.seconds'),
-//             timeInterval = setInterval(updateClock, 1000);
-
-//         function updateClock() {//вызов каждую секунду(updateClock которая получает разницу со временм при помощи фунgetTimeRemaining)
-//             let t = getTimeRemaining(endtime);
-
-//             function addZero(num) {
-//                 if (num <= 9) {
-//                     return '0' + num;
-//                 } else return num;
-//             };
-//             //записываем все данные прямо в верстку 
-//             hours.textContent = addZero(t.hours);
-//             minutes.textContent = addZero(t.minutes);
-//             seconds.textContent = addZero(t.seconds);
-
-//             if (t.total <= 0) {//остановить 
-//                 clearInterval(timeInterval);
-//                 hours.textContent = '00';
-//                 minutes.textContent = '00';
-//                 seconds.textContent = '00';
-//             }
-//         }
-
-//     }
-
-//     setClock('timer', deadline);//для того чтобы сразу увидить эту функцию
-// });
-
-class User {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
-    }
-    hello() {//методы нашего клас (то что он умеет делать)
-        console.log('Hello ${this.name}')
-    }
-    exit() {
-        console.log('Пользователь ${this.name} ушел')
-    }
-}
-let ivan = new User('Ivan', 23);
-let alex = new User('Alex', 20);
-console.log(ivan);
-console.log(alex);
-ivan.hello();
-alex.hello();
-
-
+// //1) Просто вызов функции - window/uandefind
+// //2) Метод объекта - this = объекту
+// //3) Конструктор ( new)- this = новый созданый объект
+// //4) Указание конкретного контекста - coll, apply, bind
