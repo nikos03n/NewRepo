65469117df6bc2a18d6331cca4d7d23482b11004
nikolay tamazenko <tamazenko@outlook.com>
2020-07-02 22:28:15 +0300

    Урок 4.3. Реализация скрипта отправки данных из формы (асенхронные запросы)

--- 3,2/js/script.js
+++ 3,2/js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // Timer 
 
-    let deadline = '2020-07-1';//первый нунтк задали конечную дату
+    let deadline = '2020-09-1';//первый нунтк задали конечную дату
 
     function getTimeRemaining(endtime) {//сюда будим передовать эту дату
         let t = Date.parse(endtime) - Date.parse(new Date()),//в t лужит разница в милисик
@@ -114,3 +114,54 @@ window.addEventListener('DOMContentLoaded', function () {
 // }
 
 // showUser.apply(age, ["Горький","Максим"]);
+
+// Form
+
+let message = {//объект вкотором будет содержаться различные сочтаяния нашего запрса (в будующем это может быть картинки анимации)
+    loading: 'Загрузка...',//свойства
+    success: 'Спасибо! Скоро мы с вами свяжемся!',//свойства
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),//переменные со стронице это форма, и получаем эту форму через селектор класа маин
+    input = form.getElementsByTagName('input'),//и импуты ,нам все необходимо, селектор input
+    statusMessage = document.createElement('div');//div новый добавляем
+
+statusMessage.classList.add('status');//добовляем клас новому диву
+//навешуем на кнопку обробочик события на форму form
+form.addEventListener('submit', function (event) {
+    event.preventDefault();//мы должны отменить стондартное паведение браузера (кнопку вкл) странице не будет отправлять запрос приклике на кнопку
+    form.appendChild(statusMessage);//пользователя нужно уповестить для этого на необходимо новый элемент поместить в нашу форму(добовляем новый див который лежит с названием статусмеседж)
+    //создаем наш запрос
+    let request = new XMLHttpRequest();//созд перемен и помещаем новый конструкторы
+    request.open('POST', 'server.php');//и настроить (какие продукты и др)обращаемся к серверуPOST
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');//настройки заголовка httpзапроса(это команда которая говорит что будет содержать данные полученные из формы)
+    //отправлять данные с form на сервер нужно чтобы в импутах стояли парамер name=''
+    let formData = new FormData(form);//так помещают все то что внес пользователь 
+    //request.send(formData); можно вместо let formData = new FormData(form);//передаем данные которые вел польз и уже на сервере должно быть
+    //пока для  пользователя толку нету он нечего не видит , дорабатываес скрипт
+
+    let obj = {};
+    formData.forEach(function (value, key) {
+        obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function () {//отслеживаем изменения нашего запроса
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;//если сервер тупит получ
+        } else if (request.readyState === 4 && request.status == 200) {//здесь можно ввсе и контент на странице ставить и полоску с загруской
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i = 0; i < input.length; i++) {//вычисляем количество импутов и укаждого возьмем значение и превратим в пустую строку и очистим все наши импутыы
+        input[i].value = '';
+    }
+});
+//jSON - фаилы отправляет а не обычные , создаем промежуточный объкт и используем метот форич и внутри обж заполняем знач
+//подом let json = JSON.stringify(obj);
